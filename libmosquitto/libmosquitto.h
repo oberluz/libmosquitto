5db467ea5676122b7f451ad7dd91e77655b16c32
Albin Stigö <albin.stigo@gmail.com>
2013-04-17 12:01:51 +0200

    Initial Commit

---
+++ libmosquitto/libmosquitto.h
@@ -0,0 +1,13 @@
+//
+//  libmosquitto.h
+//  libmosquitto
+//
+//  Created by Albin Stigo on 4/17/13.
+//  Copyright (c) 2013 Albin Stigo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface libmosquitto : NSObject
+
+@end
---
+++ libmosquitto/libmosquitto.m
@@ -0,0 +1,13 @@
+//
+//  libmosquitto.m
+//  libmosquitto
+//
+//  Created by Albin Stigo on 4/17/13.
+//  Copyright (c) 2013 Albin Stigo. All rights reserved.
+//
+
+#import "libmosquitto.h"
+
+@implementation libmosquitto
+
+@end
